a96e840489467e4c7e2ae8f8dda53ed0a8e86ee1
VidelaR <videlaivan10@gmail.com>
2023-01-29 13:35:46 -0300

    Prueba commit 2

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ echo("<h1 style='color:red;'> Hola Mundo </h1>");
 
 echo("<h2 style='color:orange;'>UTN-FRA</h2>");
 
-echo("<h3 style='color:yellow;'>Prueba</h3>");
+echo("<h3 style='color:yellow;'>Prueba2</h3>");
 
 ?>
